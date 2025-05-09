751cbdbde0bdedb39af69496d5738f372ec3733d
MarcpTr <marcphtr@gmail.com>
2025-05-09 22:04:19 +0200

    Create Note.java

---
+++ src/main/java/com/example/jwt_demo/model/Note.java
@@ -0,0 +1,32 @@
+package com.example.jwt_demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
