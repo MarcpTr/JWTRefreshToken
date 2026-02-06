b385694daf73fd0b8419bf27f7b95008d8c960f8
marcphtr <marcphtr@gmail.com>
2026-02-06 20:30:42 +0100

    updated

--- src/main/java/com/example/jwt_demo/controller/TestController.java
+++ src/main/java/com/example/jwt_demo/controller/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-
 import com.example.jwt_demo.model.User;
 
 @RestController
