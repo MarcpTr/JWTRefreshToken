b522491a3ec465a0a180994f56b8fe6c0ce54fb1
marcphtr <marcphtr@gmail.com>
2026-02-18 21:55:27 +0100

    updated

--- src/main/java/com/example/jwt_demo/controller/TestController.java
+++ src/main/java/com/example/jwt_demo/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.example.jwt_demo.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
