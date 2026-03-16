a6f1f72103e4eba9309851899c634132b7545f5a
marcphtr <marcphtr@gmail.com>
2026-03-16 17:24:51 +0100

    updated

--- src/main/java/com/example/jwt_demo/controller/SessionController.java
+++ src/main/java/com/example/jwt_demo/controller/SessionController.java
@@ -1,7 +1,6 @@
 package com.example.jwt_demo.controller;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
